cc9e278a03b44c1fead7de6f56b41d06ea69c2a0
lghita <ghita.lucian95@gmail.com>
2023-06-09 18:01:57 +0200

    Add working solution for multiply and almost working solution for fill

--- DevSuite/nand2tetris/projects/04/mult/Mult.asm
+++ DevSuite/nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,32 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@R2
+M=0
+@R0
+D=M
+@END
+D;JEQ
+@R1
+D=M
+@END
+D;JEQ
+@n
+M=D
+(LOOP)
+@n
+D=M
+@END
+D;JEQ
+@R0
+D=M
+@R2
+M=D+M
+@n
+M=M-1
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
