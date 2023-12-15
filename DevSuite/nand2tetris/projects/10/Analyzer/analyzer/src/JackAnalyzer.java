b5d4d1b82ff48e5fe35d5bdb5fd212322fca6241
LucianGhita <lucianghita@github.com>
2023-12-15 08:43:39 +0100

    Some work on the Jack Analyzer

---
+++ DevSuite/nand2tetris/projects/10/Analyzer/analyzer/src/JackAnalyzer.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class JackAnalyzer {
+    public static void main(String[] args) {
+        JackTokenizer tokenizer = new JackTokenizer( "/home/lgh/Programming/Nand2tetris/Nand2tetris/DevSuite/nand2tetris/projects/10/ArrayTest/Main.jack");
+    }
+
+
+}
